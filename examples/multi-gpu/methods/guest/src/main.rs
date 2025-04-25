bfc13a61c769c1cd6f0720e74be94f2342f83ce7
Richard Howard <rich@risczero.com>
2025-04-24 17:54:49 -0700

    hmm

--- examples/multi-gpu/methods/guest/src/main.rs
+++ examples/multi-gpu/methods/guest/src/main.rs
@@ -9,15 +9,21 @@ risc0_zkvm::guest::entry!(main);
 fn main() {
     let mut counter = 0;
 
-    // Do a bunch of iterations to generate multiple segments
-    for i in 0..1000 {
+    // Do a bunch of iterations with pauses to generate exactly 4 segments
+    for i in 0..400 {
         counter += i;
 
-        // Periodically add a pause point to create new segments
-        if i % 100 == 0 {
-            // Use a static string instead of format! which may rely on alloc
-            env::log("Adding pause point");
-            env::pause();
+        // Pause at specific points to create exactly 3 pauses (4 segments total)
+        if i == 100 || i == 200 || i == 300 {
+            // Use simple static log messages to avoid format! which requires alloc
+            if i == 100 {
+                env::log("Creating segment 1");
+            } else if i == 200 {
+                env::log("Creating segment 2");
+            } else {
+                env::log("Creating segment 3");
+            }
+            env::pause(0);
         }
     }
 
